43372e183f08654d56d801ed8c0f517b1b9b42d5
Rados <rashke89@gmail.com>
2022-07-27 19:51:10 +0200

    react version

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.min.js";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./redux/store";
 import "animate.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
         </BrowserRouter>
-    </React.StrictMode>
+    </React.StrictMode>,
+    root
 );
 
 // If you want to start measuring performance in your app, pass a function
