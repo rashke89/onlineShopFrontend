a7aa5bb2d23fa17703fa561c5b8d1bc850aa13a4
Rados <rashke89@gmail.com>
2022-07-27 20:02:58 +0200

    slick slider version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,16 +9,15 @@ import {Provider} from "react-redux";
 import store from "./redux/store";
 import "animate.css"
 
-const root = document.getElementById('root');
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
         </BrowserRouter>
-    </React.StrictMode>,
-    root
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
